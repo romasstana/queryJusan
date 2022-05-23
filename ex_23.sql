f1415bbbf73b92eb60d829c9d8f6f0acf31c69a0
romasstana <42274229+romasstana@users.noreply.github.com>
2022-05-23 16:57:30 +0600

    Create ex_23.sql

---
+++ ex_23.sql
@@ -0,0 +1,6 @@
+SELECT T.name, Quantity from InvoiceLine
+join Track T on T.TrackId = InvoiceLine.TrackId
+join Invoice I on I.InvoiceId = InvoiceLine.InvoiceId
+where strftime('%Y', InvoiceDate)='2013'
+group by T.Name
+order by Quantity desc;
