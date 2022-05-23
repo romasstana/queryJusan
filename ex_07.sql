90397abe026718590a97734427698ed6517921d5
romasstana <42274229+romasstana@users.noreply.github.com>
2022-05-23 16:52:57 +0600

    Create ex_07.sql

---
+++ ex_07.sql
@@ -0,0 +1,3 @@
+SELECT I.total, (Customer.FirstName || ' ' || Customer.LastName) as Name, I.billingCountry, (E.FirstName || ' ' || E.LastName) as Employee from Customer
+JOIN Employee E on E.EmployeeId = Customer.SupportRepId
+JOIN Invoice I on Customer.CustomerId = I.CustomerId;
