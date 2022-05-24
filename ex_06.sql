b01ade643a6ccbc11195485943bccf794842e972
romasstana <42274229+romasstana@users.noreply.github.com>
2022-05-24 15:25:38 +0600

    Update ex_06.sql

--- ex_06.sql
+++ ex_06.sql
@@ -1,3 +1,4 @@
-SELECT C.SupportRepId, (Employee.FirstName || ' ' || Employee.LastName) as Name, Title AS Name from Employee
-Join Customer C on Employee.EmployeeId = C.SupportRepId
-GROUP BY Name;
+SELECT i.InvoiceId, (e.FirstName || ' ' || e.LastName) FROM Invoice i
+JOIN Customer c ON  i.CustomerId = c.CustomerId
+JOIN Employee e ON c.CustomerId = e.EmployeeId
+Where e.Title LIKE '%agent%';
