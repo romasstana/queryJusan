413331c59a2f7a9f60e8425e1b3008d4d2aa46d6
romasstana <42274229+romasstana@users.noreply.github.com>
2022-05-24 15:12:04 +0600

    Update ex_03.sql

--- ex_03.sql
+++ ex_03.sql
@@ -1,2 +1,2 @@
 SELECT (FirstName || ' ' || LastName), InvoiceId, InvoiceDate, BillingCountry From Customer c join Invoice I on c.CustomerId = I.CustomerId
-WHERE BillingCountry='Brazil';
+WHERE Country='Brazil';
