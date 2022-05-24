3bd859d037d9658c4fa1a11a67879de2456dc427
romasstana <42274229+romasstana@users.noreply.github.com>
2022-05-24 15:35:29 +0600

    Update ex_12.sql

--- ex_12.sql
+++ ex_12.sql
@@ -1,4 +1,5 @@
 SELECT InvoiceId, T.name, A2.name from InvoiceLine
 join Track T on T.TrackId = InvoiceLine.TrackId
 join Album A on T.AlbumId = A.AlbumId
-join Artist A2 on A.ArtistId = A2.ArtistId;
+join Artist A2 on A.ArtistId = A2.ArtistId
+group by t.name;
