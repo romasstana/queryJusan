a67e254ba96c57d28023404c648a600b57ce2110
romasstana <42274229+romasstana@users.noreply.github.com>
2022-05-24 15:32:04 +0600

    Update ex_07.sql

--- ex_07.sql
+++ ex_07.sql
@@ -1,3 +1,4 @@
 SELECT I.total, (Customer.FirstName || ' ' || Customer.LastName) as Name, I.billingCountry, (E.FirstName || ' ' || E.LastName) as Employee from Customer
 JOIN Employee E on E.EmployeeId = Customer.SupportRepId
-JOIN Invoice I on Customer.CustomerId = I.CustomerId;
+JOIN Invoice I on Customer.CustomerId = I.CustomerId
+where title like '%agent%';
