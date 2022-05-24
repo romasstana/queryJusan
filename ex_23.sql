18bccaab7754fc5168e1758d0749a9d6062ccf46
romasstana <42274229+romasstana@users.noreply.github.com>
2022-05-24 15:59:51 +0600

    Update ex_23.sql

--- ex_23.sql
+++ ex_23.sql
@@ -1,6 +1,6 @@
-SELECT T.name, Quantity from InvoiceLine
+SELECT T.name, count(Quantity) from InvoiceLine
 join Track T on T.TrackId = InvoiceLine.TrackId
 join Invoice I on I.InvoiceId = InvoiceLine.InvoiceId
 where strftime('%Y', InvoiceDate)='2013'
 group by T.Name
-order by Quantity desc;
+order by count(Quantity) desc;
