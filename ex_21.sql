6b28007d3bc8b84dd9b0da9b39571f91326016f0
romasstana <42274229+romasstana@users.noreply.github.com>
2022-05-24 15:57:11 +0600

    Update ex_21.sql

--- ex_21.sql
+++ ex_21.sql
@@ -1,5 +1,3 @@
-SELECT count(I.customerId), (e.FirstName || ' ' || e.LastName) from Employee e
+SELECT count(customerId), (e.FirstName || ' ' || e.LastName) from Employee e
 Join Customer C on e.EmployeeId = C.SupportRepId
-Join Invoice I on C.CustomerId = I.CustomerId
-WHERE e.Title like '%sales%' or '%agent%'
-group by e.EmployeeId;
+group by SupportRepId;
